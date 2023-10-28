6841142f8b1b20c3a4830e3248ec0390d5258b71
saulosgil <saulosgil@hotmail.com>
2023-10-28 17:30:39 -0300

    grafico de barras e regressoes crude e ajustadas

--- analises.R
+++ analises.R
@@ -1,5 +1,6 @@
 # pacotes -------------------------------------------------------------------------------------
 library(tidyverse)
+library(corrplot)
 library(patchwork)
 
 # ler a base ----------------------------------------------------------------------------------
@@ -22,57 +23,10 @@ hig_strength <-
 
 df <- bind_rows(low_strength,hig_strength)
 
-# Criando os grupos pela mediana de acordo com o TUG -------------------------------------------------------
-df <- df |>
-  arrange(tug_max)
-
-low_tug <-
-  df |>
-  slice(1:100) |>
-  mutate(tug_class = "A_hig_tug")
-
-hig_tug <-
-  df |>
-  slice(101:200) |>
-  mutate(tug_class = "B_low_tug")
-
-
-df <- bind_rows(low_tug,hig_tug)
-
-# Criando os tercis de acordo com o TS --------------------------------------------------------
-df <- df |>
-  arrange(ts_max)
-
-low_ts <-
-  df |>
-  slice(1:100) |>
-  mutate(ts_class = "B_low_ts")
-
-hig_ts <-
-  df |>
-  slice(101:200) |>
-  mutate(ts_class = "A_hig_ts")
-
-df <- bind_rows(low_ts,hig_ts)
-
-# Criando as classes para ansiedade, depressão, idade, bmi -----------------------------------
+# Criando as classes para idade, bmi -----------------------------------
 df <-
   df |>
   mutate(
-    ansiedade_class = case_when(
-      ansiedade_score <  08 ~ "Minimal",
-      ansiedade_score >= 08 & ansiedade_score < 16 ~ "Mild",
-      ansiedade_score >= 16 & ansiedade_score < 26 ~ "Moderate",
-      ansiedade_score >= 26 ~ "Severe"
-    ),
-    depressao_class = case_when(
-      depressao_score <  14 ~ "Minimal",
-      depressao_score >= 14 & depressao_score < 20 ~ "Mild",
-      depressao_score >= 20 & depressao_score < 29 ~ "Moderate",
-      depressao_score >= 29 ~ "Severe"
-    ),
-    ansiedade_class_2 = if_else(ansiedade_score < 16, "Minimal/Mild","Moderate/Severe"),
-    depressao_class_2 = if_else(depressao_score < 20, "Minimal/Mild","Moderate/Severe"),
     idade_class = if_else(idade < 65, "adulto", "idoso"),
     bmi = peso/((estatura/100)^2),
     bmi_class = if_else(bmi < 30, "non-obese", "obese",missing = "non-obese")
@@ -81,329 +35,200 @@ df <-
 # realocando as colunas -----------------------------------------------------------------------
 df <-
   df|>
+  select(
+    -percentual_gordura,
+    -percentual_mm,
+    -tug_max,
+    -ts_max
+  ) |>
   relocate(hgs_class,.after = hgs_max) |>
-  relocate(tug_class,.after = tug_max) |>
-  relocate(ts_class,.after = ts_max) |>
-  relocate(ansiedade_class,.after = ansiedade_score) |>
-  relocate(ansiedade_class_2,.after = ansiedade_class) |>
-  relocate(depressao_class,.after = ansiedade_class_2) |>
-  relocate(depressao_class_2,.after = depressao_class) |>
   relocate(idade_class,.after = idade) |>
   relocate(bmi,.after = estatura) |>
   relocate(bmi_class,.after = bmi) |>
-  mutate(raca = as.factor(raca)) |>
-  mutate(ansiedade_class = as.factor(ansiedade_class),
-         ansiedade_class_2 = as.factor(ansiedade_class_2),
-         depressao_class = as.factor(depressao_class),
-         depressao_class_2 = as.factor(depressao_class_2))
+  mutate(raca = as.factor(raca))
 
 glimpse(df)
 skimr::skim(df)
 
+
 # Graficos exploratórios ----------------------------------------------------------------------
+
 df |>
-  ggplot(mapping = aes(x = hgs_max,y = ansiedade_score)) +
+  ggplot(mapping = aes(x = hgs_max,y = whoqol_fisico_escore_100)) +
+  geom_point()+
+
+df |>
+  ggplot(mapping = aes(x = hgs_max,y = whoqol_psicol_escore_100)) +
   geom_point()
 
 df |>
-  ggplot(mapping = aes(x = tug_max,y = ansiedade_score)) +
+  ggplot(mapping = aes(x = hgs_max,y = whoqol_social_escore_100)) +
   geom_point()
 
 df |>
-  ggplot(mapping = aes(x = ts_max,y = ansiedade_score)) +
+  ggplot(mapping = aes(x = hgs_max,y = whoqol_ambiente_escore_100)) +
   geom_point()
 
 df |>
-  ggplot(mapping = aes(x = hgs_max,y = depressao_score)) +
+  ggplot(mapping = aes(x = hgs_max,y = who_percep_saude)) +
   geom_point()
 
 df |>
-  ggplot(mapping = aes(x = tug_max,y = depressao_score)) +
+  ggplot(mapping = aes(x = hgs_max,y = who_satisf_saude)) +
   geom_point()
 
-df |>
-  ggplot(mapping = aes(x = ts_max,y = depressao_score)) +
-  geom_point()
 
 # Grafico de barras ---------------------------------------------------------------------------
-df |>
+# Physical domain
+g1 <-
+  df |>
   group_by(hgs_class) |>
-  summarize(media = mean(depressao_score),
-            DP = sd(depressao_score)) |>
+  summarize(media = mean(whoqol_fisico_escore_100, na.rm = TRUE),
+            DP = sd(whoqol_fisico_escore_100, na.rm = TRUE)) |>
   ggplot(mapping = aes(x = hgs_class,
                        y = media)) +
   geom_bar(position = position_dodge(),
            stat = "identity",
            colour = "black",
-           fill = c("black", "white"),
+           fill = c("#0000e6", "#ff0000"),
            show.legend = FALSE) +
   geom_errorbar(aes(ymin = media,
                     ymax = media + DP),
                 width=.2) +
   theme_classic() +
-  ylab(label = "Beck depression inventory score (a.u.)") +
-  scale_x_discrete(labels = c("High\nTimed Stands", "Low\nTimed Stands"))+
+  ylab(label = "Physical domain (a.u.)") +
+  scale_x_discrete(labels = c("High\nStrength", "Low\nStrength"))+
   theme(
     axis.title.x = element_blank()
   ) +
-  scale_y_continuous(limits = c(0, 25, 5))
+  scale_y_continuous(limits = c(0, 100, 25))
 
-# Grafico de barras para mostrar frequencia relativa de sintomas severos de ansiedade  --------
-# handgrip ------------------------------------------------------------------------------------
-# Ansiedade
-hgs_ansiedade_severa <-
+# Psychological domain
+g2 <-
   df |>
-  group_by(hgs_class, ansiedade_class_2) |>
-  summarise(abs_ansiedade_2 = n(),
-            perc_ansiedade_2 = n()/(nrow(df)/2)*100)
-
-hgs_ansiedade_severa
-
-# vetor de ordenação
-
-level_order <- c("Minimal/Mild", "Moderate/Severe")
-# plot
-hgs_ans <-
-  df |>
-  group_by(hgs_class, ansiedade_class_2) |>
-  summarise(abs_ansiedade_2 = n(),
-            perc_ansiedade_2 = n()/(nrow(df)/2)*100) |>
-  ggplot(mapping = aes(x = ansiedade_class_2,
-                       y = perc_ansiedade_2,
-                       colour = "black",
-                       fill = hgs_class)) +
-  geom_col(position = "dodge",
-           colour = "black") +
-  geom_text(aes(
-    label = c("95%", "13%", "86%", "22%")),
-    color = "black",
-    position = position_dodge(0.9),
-    size = 8,
-    vjust = -0.5,
-    show.legend = FALSE) +
-  scale_x_discrete(limits = level_order) +
-  scale_y_continuous(limits = c(0, 100, 25)) +
-  scale_fill_manual(labels = c("High Strength", "Low Strength"),
-                    values = c("black", "white")) +
-  xlab(label = "") +
-  ylab("relative frequency (%)") +
+  group_by(hgs_class) |>
+  summarize(media = mean(whoqol_psicol_escore_100, na.rm = TRUE),
+            DP = sd(whoqol_psicol_escore_100, na.rm = TRUE)) |>
+  ggplot(mapping = aes(x = hgs_class,
+                       y = media)) +
+  geom_bar(position = position_dodge(),
+           stat = "identity",
+           colour = "black",
+           fill = c("#0000e6", "#ff0000"),
+           show.legend = FALSE) +
+  geom_errorbar(aes(ymin = media,
+                    ymax = media + DP),
+                width=.2) +
   theme_classic() +
+  ylab(label = "Psychological domain(a.u.)") +
+  scale_x_discrete(labels = c("High\nStrength", "Low\nStrength"))+
   theme(
-    legend.position = "top",
-    legend.title = element_blank()
-  )
+    axis.title.x = element_blank()
+  ) +
+  scale_y_continuous(limits = c(0, 100, 25))
 
-hgs_ans
-
-# Depressao
-ts_depressao_severa <-
+# Social domain
+g3 <-
   df |>
-  group_by(ts_class, depressao_class_2) |>
-  summarise(abs_depressao_2 = n(),
-            perc_depressao_2 = n()/(nrow(df)/2)*100)
-
-ts_depressao_severa
-
-# Plot
-hgs_dep <-
-  df |>
-  group_by(hgs_class, depressao_class_2) |>
-  summarise(abs_depressao_severo_2 = n(),
-            perc_depressao_severo_2 = n()/(nrow(df)/2)*100) |>
-  ggplot(mapping = aes(x = depressao_class_2,
-                       y = perc_depressao_severo_2,
-                       colour = "black",
-                       fill = hgs_class)) +
-  geom_col(position = "dodge",
-           colour = "black") +
-  geom_text(aes(
-    label = c("95%","13%","92%","16%")),
-    color = "black",
-    position = position_dodge(0.9),
-    size = 8,
-    vjust = -0.5,
-    show.legend = FALSE) +
-  scale_x_discrete(limits = level_order) +
-  scale_y_continuous(limits = c(0, 100, 25)) +
-  scale_fill_manual(labels = c("High Strength", "Low Strength"),
-                    values = c("black", "white")) +
-  xlab(label = "") +
-  ylab("relative frequency (%)") +
+  group_by(hgs_class) |>
+  summarize(media = mean(whoqol_social_escore_100, na.rm = TRUE),
+            DP = sd(whoqol_social_escore_100, na.rm = TRUE)) |>
+  ggplot(mapping = aes(x = hgs_class,
+                       y = media)) +
+  geom_bar(position = position_dodge(),
+           stat = "identity",
+           colour = "black",
+           fill = c("#0000e6", "#ff0000"),
+           show.legend = FALSE) +
+  geom_errorbar(aes(ymin = media,
+                    ymax = media + DP),
+                width=.2) +
   theme_classic() +
+  ylab(label = "Social domain (a.u.)") +
+  scale_x_discrete(labels = c("High\nHigh Strength", "Low\nStrength"))+
   theme(
-    legend.position = "top",
-    legend.title = element_blank()
-  )
+    axis.title.x = element_blank()
+  ) +
+  scale_y_continuous(limits = c(0, 100, 25))
 
-hgs_dep
-
-# Timed stands --------------------------------------------------------------------------------
-# Ansiedade
-ts_ansiedade_severa <-
+# Environment domain
+g4 <-
   df |>
-  group_by(ts_class, ansiedade_class_2) |>
-  summarise(abs_ansiedade_2 = n(),
-            perc_ansiedade_2 = n()/(nrow(df)/2)*100)
-
-ts_ansiedade_severa
-
-# Qui quadrado
-ts_ans_chisquare <- table(df$ts_class, df$ansiedade_class)
-chisq.test(ts_ans_chisquare)
-
-# plot
-ts_ans <-
-  df |>
-  group_by(ts_class, ansiedade_class) |>
-  summarise(abs_ansiedade_severo = n(),
-            perc_ansiedade_severo = n()/(nrow(df)/2)*100) |>
-  ggplot(mapping = aes(x = ansiedade_class,
-                       y = perc_ansiedade_severo,
-                       colour = "black",
-                       fill = ts_class)) +
-  geom_col(position = "dodge",
-           colour = "black") +
-  geom_text(aes(
-    label = c("13.9%","76.9%","7.4%","1.8%","24.1%","52.8%","17.6%","5.5%")),
-    color = "black",
-    position = position_dodge(0.9),
-    size = 5,
-    vjust = -0.5,
-    show.legend = FALSE) +
-  scale_x_discrete(limits = level_order) +
-  scale_y_continuous(limits = c(0, 100, 25)) +
-  scale_fill_manual(labels = c("High Timed Stands", "Low Timed Stands"),
-                    values = c("black", "white")) +
-  xlab(label = "") +
-  ylab("relative frequency (%)") +
+  group_by(hgs_class) |>
+  summarize(media = mean(whoqol_ambiente_escore_100, na.rm = TRUE),
+            DP = sd(whoqol_ambiente_escore_100, na.rm = TRUE)) |>
+  ggplot(mapping = aes(x = hgs_class,
+                       y = media)) +
+  geom_bar(position = position_dodge(),
+           stat = "identity",
+           colour = "black",
+           fill = c("#0000e6", "#ff0000"),
+           show.legend = FALSE) +
+  geom_errorbar(aes(ymin = media,
+                    ymax = media + DP),
+                width=.2) +
   theme_classic() +
+  ylab(label = "Environment domain (a.u.)") +
+  scale_x_discrete(labels = c("High\nStrength", "Low\nStrength"))+
   theme(
-    legend.position = "top",
-    legend.title = element_blank()
-  )
+    axis.title.x = element_blank()
+  ) +
+  scale_y_continuous(limits = c(0, 100, 25))
 
-ts_ans
+# layout
+(g1+g2)/(g3+g4)
 
-# Depressao
-ts_depressao_severa <-
-  df |>
-  group_by(ts_class, depressao_class) |>
-  summarise(abs_depressao_severo = n(),
-            perc_depressao_severo = n()/(nrow(df)/2)*100)
-
-ts_depressao_severa
-
-# Qui quadrado
-ts_dep_chisquare <- table(df$ts_class, df$depressao_class)
-chisq.test(ts_dep_chisquare)
-
-# Plot
-ts_dep <-
-  df |>
-  group_by(ts_class, depressao_class) |>
-  summarise(abs_depressao_severo = n(),
-            perc_depressao_severo = n()/(nrow(df)/2)*100) |>
-  ggplot(mapping = aes(x = depressao_class,
-                       y = perc_depressao_severo,
-                       colour = "black",
-                       fill = ts_class)) +
-  geom_col(position = "dodge",
-           colour = "black") +
-  geom_text(aes(
-    label = c("7.4%","81.5%","8.3%","2.7%","18.5%","65.7%","9.2%","6.4%")),
-    color = "black",
-    position = position_dodge(0.9),
-    size = 5,
-    vjust = -0.5,
-    show.legend = FALSE) +
-  scale_x_discrete(limits = level_order) +
-  scale_y_continuous(limits = c(0, 100, 25)) +
-  scale_fill_manual(labels = c("High Strength", "Low Strength"),
-                    values = c("black", "white")) +
-  xlab(label = "") +
-  ylab("relative frequency (%)") +
-  theme_classic() +
-  theme(
-    legend.position = "top",
-    legend.title = element_blank()
-  )
-
-ts_dep
-
-# Layout
-(hgs_ans | ts_ans)/(hgs_dep | ts_dep)
 
 # analises exploratórias - test t e regressão -------------------------------------------------
 # tirar notação cientifica
 options(scipen=999)
 
 # Graficos + testes t para amostras independentes ---------------------------------------------
-# Ansiedade
-## HGS
-t.test(ansiedade_score ~ hgs_class, df)
+## physical domain
+t.test(whoqol_fisico_escore_100 ~ hgs_class, data = df)
 
-## TS
-t.test(ansiedade_score ~ ts_class, df)
+## Psycological domain
+t.test(whoqol_psicol_escore_100 ~ hgs_class, data = df)
 
-# Depressao
-## HGS
-t.test(depressao_score ~ hgs_class, df)
+## social domain
+t.test(whoqol_social_escore_100 ~ hgs_class, data = df)
 
-## TS
-t.test(depressao_score ~ ts_class, df)
+## environment domain
+t.test(whoqol_ambiente_escore_100 ~ hgs_class, data = df)
 
 # Regressões lineares -------------------------------------------------------------------------
-## HGS
-### Ansiedade
+## Physical domain
 #### Crude
-model <- lm(ansiedade_score ~ hgs_max, df)
+model <- lm(whoqol_fisico_escore_100 ~ hgs_max, df)
 sjPlot::tab_model(model)
 
-#### Ajustado 1
-model <- lm(ansiedade_score ~ hgs_max + genero + idade_class + raca + renda + bmi_class, df)
+#### Ajustado
+model <- lm(whoqol_fisico_escore_100 ~ hgs_max + genero + idade_class + raca + renda + bmi_class, df)
 sjPlot::tab_model(model)
 
-#### Ajustado 2
-model <- lm(ansiedade_score ~ hgs_max + genero + idade_class + raca + renda, df)
-sjPlot::tab_model(model)
-
-### Depressao
+## Psycological domain
 #### Crude
-model <- lm(depressao_score ~ hgs_max, df)
+model <- lm(whoqol_psicol_escore_100 ~ hgs_max, df)
 sjPlot::tab_model(model)
 
-#### Ajustado 1
-model <- lm(depressao_score ~ hgs_max + genero + idade_class + raca + renda + bmi_class, df)
+#### Ajustado
+model <- lm(whoqol_psicol_escore_100 ~ hgs_max + genero + idade_class + raca + renda + bmi_class, df)
 sjPlot::tab_model(model)
 
-#### Ajustado 2
-model <- lm(depressao_score ~ hgs_max + genero + idade_class + raca + renda, df)
-sjPlot::tab_model(model)
-
-## TS
-### Ansiedade
+## social domain
 #### Crude
-model <- lm(ansiedade_score ~ ts_max, df)
+model <- lm(whoqol_social_escore_100 ~ hgs_max, df)
 sjPlot::tab_model(model)
 
-#### Ajustado 1
-model <- lm(ansiedade_score ~ ts_max + genero + idade_class + raca + renda + bmi_class, df)
+#### Ajustado
+model <- lm(whoqol_social_escore_100 ~ hgs_max + genero + idade_class + raca + renda + bmi_class, df)
 sjPlot::tab_model(model)
 
-#### Ajustado 2
-model <- lm(ansiedade_score ~ ts_max + genero + idade_class + raca + renda, df)
-sjPlot::tab_model(model)
-
-### Depressao
+## Environment domain
 #### Crude
-model <- lm(depressao_score ~ ts_max, df)
+model <- lm(whoqol_ambiente_escore_100 ~ hgs_max, df)
 sjPlot::tab_model(model)
 
-#### Ajustado 1
-model <- lm(depressao_score ~ ts_max + genero + idade_class + raca + renda + bmi_class, df)
+#### Ajustado
+model <- lm(whoqol_ambiente_escore_100 ~ hgs_max + genero + idade_class + raca + renda + bmi_class, df)
 sjPlot::tab_model(model)
-
-#### Ajustado 2
-model <- lm(depressao_score ~ ts_max + genero + idade_class + raca + renda, df)
-sjPlot::tab_model(model)
-
